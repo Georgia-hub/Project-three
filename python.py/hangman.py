08b8c650cf9a0621f2cba4cbb0851c88a6557f38
Georgia Kershaw <georgiakershaw8@hotmail.com>
2021-12-21 20:11:45 +0000

    added comments to planing game

---
+++ python.py/hangman.py
@@ -0,0 +1,15 @@
+# Import words
+
+def get-wor
+
+# set lives 
+# start the game
+# choose words
+# hide words with underscores
+# user import
+# if correct - reveal letter
+# else loose a life - diplay a line in hangman
+# continue until lives have gone
+# if all letters are guessed then the man survives
+# else loose and the man dies
+# reset the game
